f5ebb9d7d268f0e4f8295646508b9f2183663d66
kevinhsu <kevinhsu@synology.com>
2017-03-08 13:47:30 +0000

    define apis

--- deoa-register-backend/server.js
+++ deoa-register-backend/server.js
@@ -19,10 +19,41 @@ app.get("/user", function(req, res) {
 	const users = [{
 		"name": "mimi",
 		"mail": "lo.aiming@gmail.com",
-		"passwd": "passwd"
+		"passwd": "some hashed passwd" // will not be given in GET
 	}];
 	res.status(200).json(users);
-})
+});
+
+app.get("/event", function(req, res) {
+	const events = [{
+		"name": "paseminar",
+		"description": "pa seminar description",
+		"place": [{
+			"location": "NTU",
+			"date": 1488979285,
+			"limit": 30,
+			"meta": "in NTU"
+		}, {
+			"location": "Eslite",
+			"date": 1488979504,
+			"limit": 40,
+			"meta": "in Eslite"
+		}]
+	}];
+	res.status(200).json(events);
+});
+
+app.get("/register", function(req, res) {
+	const registers = [{
+		"mail": "lo.aiming@gmail.com",
+		"register": [{
+			"name": "paseminar",
+			"location": "NTU",
+			"date": 1488979285
+		}]
+	}];
+	res.status(200).json(registers);
+});
 
 var server = app.listen(8000, function () {
 	console.log("Listening on port %s...", server.address().port);
